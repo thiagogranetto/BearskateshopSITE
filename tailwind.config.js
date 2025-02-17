55d5a2818caac3dbfaacc6656656ad1edbb14e95
ThiagoGranetto <granettot@gmail.com>
2025-02-17 15:46:16 -0300

    frontend 20%

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sidger: ['sidger', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
